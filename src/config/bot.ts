dd1f37cd2d27aa9c7ce0c08b5ae6b99fc98d88cb
banana0616 <ban061692@gmail.com>
2021-01-20 15:10:25 +0900

    DisKord

---
+++ src/config/bot.ts
@@ -0,0 +1,6 @@
+export default {
+    token: '',
+    shards: {
+        token: ''
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import {ShardingManager} from 'discord.js';
 import path from 'path';
+import config from './config'
 
-const manager = new ShardingManager(path.join(__dirname, 'bot.ts'), )
+const manager = new ShardingManager(path.join(__dirname, 'bot.ts'), config.bot.shards)
 
 manager.on('shardCreate', (Shard) => console.log(`[Shard] Shard #${Shard.id} Create`))
 
